51dec67c73c8b2f957658fed4e2c7052d91d07e2
Kai R <kairichardson2000@gmail.com>
2019-09-28 15:24:37 -0700

    current production build

--- routes/api/videos.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const videoController = require("../../controllers/videoController");
-
-// Matches with "/api/videos"
-router.route("/:searchWord").get(videoController.searchAll);
-router.route("/").get(videoController.returnAll);
-
-module.exports = router;
